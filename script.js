80e0273926f9154f8c6346acb6f1fc737293c1ac
Alex Agapov <aagapov.jr@gmail.com>
2017-02-09 14:30:00 +0300

    brush select; minor changes

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 const TILE_SIZE = 32;
-const MAP_SIZE_X = 30;
-const MAP_SIZE_Y = 20;
+const MAP_SIZE_X = 10;
+const MAP_SIZE_Y = 10;
 
 window.onload = function () {
 
 	var canvas = document.getElementById('myCanvas');
 	var context = canvas.getContext('2d');
 	context.drawTile = function (tilename, x, y) {
-		this.drawImage(images.get(tilename), x*TILE_SIZE, y*TILE_SIZE);
+		this.drawImage(brushes.get(tilename), x*TILE_SIZE, y*TILE_SIZE);
 	}
 
 	canvas.width = MAP_SIZE_X*TILE_SIZE;
 	canvas.height = MAP_SIZE_Y*TILE_SIZE;
 
-	var images = new Map;
-	var imagesDiv = document.getElementById('images');
-	var imageObjects = imagesDiv.getElementsByTagName('img');
+	var brushes = new Map;
+	var brushesDiv = document.getElementById('brushes');
+	var brushObjects = brushesDiv.getElementsByTagName('img');
 
-	for (var i=0; i<imageObjects.length; i++) {
-		images.set(imageObjects[i].id, imageObjects[i]);
+	var selectedBrushValue;
+
+	for (var i=0; i<brushObjects.length; i++) {
+		brushes.set(brushObjects[i].id, brushObjects[i]); // add brush to the list
+		brushObjects[i].addEventListener('click', function (event) {
+			selectedBrushValue = this.id;
+		})
 	}
-
+	
+	selectedBrushValue = brushes.keys().next().value;
+	// console.log(selectedBrushValue);
+	
 	canvas.addEventListener('click', function (event) {
 		var rect = canvas.getBoundingClientRect();
-		// alert (canvas);
 		var x = Math.floor((event.clientX-rect.left)/TILE_SIZE);
 		var y = Math.floor((event.clientY-rect.top)/TILE_SIZE);
-		// alert(x + '\n' + y);
-		context.drawTile('forest', x, y);
+		context.drawTile(selectedBrushValue, x, y);
 	});
 }
